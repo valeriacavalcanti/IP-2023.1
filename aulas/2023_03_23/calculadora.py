29d032c848ce7bdccf02ae33e9a4de0daeea6efb
Valéria Cavalcanti <valeria.cavalcanti@ifpb.edu.br>
2023-03-24 05:40:06 -0300

    Aula 23/03/2023

---
+++ aulas/2023_03_23/calculadora.py
@@ -0,0 +1,14 @@
+n1 = input('Primeiro valor: ')
+n2 = input('Segundo valor: ')
+n1, n2 = int(n1), int(n2)
+n3 = int(input("Terceiro valor: "))
+
+# cálculo da média aritmética
+#soma = n1 + n2 + n3
+#media = soma / 3
+media = (n1 + n2 + n3) / 3
+
+#print(n1, n2, n3)
+#print('Soma:', soma)
+#print('Média:', media)
+print('Média:', int(media))
---
+++ aulas/2023_03_23/calendario.py
@@ -0,0 +1,13 @@
+dia = input("Dia: ")
+mes = input("Mês: ")
+ano = input("Ano: ")
+
+#print("Data", end=": \n")
+#print("Data", end="\n"*4)
+print("Data\n", end=": \n")
+#print("Data: ")
+
+print(dia, mes, ano, sep="/")
+#print(dia, mes, ano, sep="")
+#print(dia, mes, ano, sep="ifpb")
+#print(dia, mes, ano, sep="\n")
